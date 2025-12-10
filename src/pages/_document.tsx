faa8877d636d7ebdded932b18be3b6f99a42d7c4
FarzanaAbbas <farzu502@gmail.com>
2025-12-10 09:11:41 +0530

    changedGSC

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import {
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+           <meta name="google-site-verification" content="RvyIddRCrCZzl4NajhpQaw2PeC6p639d223cby_ADms" />
+        </Head>
+    
       <body className="antialiased">
         <Main />
         <NextScript />
